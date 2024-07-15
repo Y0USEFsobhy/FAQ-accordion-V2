c45f81c2b54b5f3263b2f26b18553a3d0be3f119
Yousef Sobhy <145506653+Y0USEFsobhy@users.noreply.github.com>
2024-07-16 02:29:00 +0300

    Update main.js
    Optimized file to reduce code from 65 to 32 lines.

--- FAQs V2/js/main.js
+++ FAQs V2/js/main.js
@@ -1,48 +1,18 @@
-
 let iconPlusList = document.querySelectorAll(".icon-plus");
 let iconMinusList = document.querySelectorAll(".icon-minus");
-let iconsList = document.querySelectorAll(".icons");
-let questionList = document.querySelectorAll(".question");
-let Qlist = document.querySelectorAll(".Q");
+let qList = document.querySelectorAll(".Q");
+let line = document.querySelectorAll(".line");
 
-iconsList.forEach((icon, index) => {
+line.forEach((icon, index) => {
   icon.addEventListener("click", function () {
     let iconPlus = iconPlusList[index];
     let iconMinus = iconMinusList[index];
-    let q = Qlist[index];
+    let q = qList[index];
 
-    if (q.classList.contains("close")){
+    if (q.classList.contains("close")) {
       q.classList.remove("close");
       q.classList.add("open");
-    }else{
-      q.classList.remove("open");
-      q.classList.add("close");
-    }
-
-    if (iconPlus.classList.contains("not-active")) {
-      iconPlus.classList.remove("not-active");
-      iconPlus.classList.add("active");
-      iconMinus.classList.remove("active");
-      iconMinus.classList.add("not-active");
     } else {
-      iconMinus.classList.remove("not-active");
-      iconMinus.classList.add("active");
-      iconPlus.classList.remove("active");
-      iconPlus.classList.add("not-active");
-      
-    }
-  });
-});
-questionList.forEach((question, index) => {
-    question.addEventListener("click", function () {
-    let iconPlus = iconPlusList[index];
-    let iconMinus = iconMinusList[index];
-    let q = Qlist[index];
-
-    if (q.classList.contains("close")){
-      q.classList.remove("close");
-      q.classList.add("open");
-    }else{
       q.classList.remove("open");
       q.classList.add("close");
     }
@@ -60,4 +30,3 @@ questionList.forEach((question, index) => {
     }
   });
 });
-
